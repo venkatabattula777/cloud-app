a299efd9211ae8185c1ce12307a9e056bc76cf28
venkatabattula777 <98868659+venkatabattula777@users.noreply.github.com>
2025-01-23 12:08:26 +0530

    Done Changes
    Done Changes

--- greet-service/src/main/java/com/demo/spring/GreetServiceApplication.java
+++ greet-service/src/main/java/com/demo/spring/GreetServiceApplication.java
@@ -2,12 +2,21 @@ package com.demo.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
+@RequestMapping("/api")
 public class GreetServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreetServiceApplication.class, args);
 	}
 
+	@GetMapping("/greet")
+	public String greet() {
+		return "If you see me, you have done it!!";
+	}
 }
